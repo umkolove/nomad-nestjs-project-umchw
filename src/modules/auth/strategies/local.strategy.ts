bb411ca7991664ebe19fd3b876d4fe8b6b4df5b5
umkolove <umarbedelbaev@gmail.com>
2024-04-26 15:27:12 +0600

    add module

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -5,6 +5,13 @@ import { Strategy } from 'passport-local';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
+  private readonly FAILE: number = 3;
+  private readonly time: number = 10 * 60 * 1000; 
+
+
+  private failedLoginAttempts: Map<string, number> = new Map();
+  private blockedUsers: Set<string> = new Set();
+
   constructor(private readonly authService: AuthService) {
     super({
       usernameField: 'email',
@@ -12,10 +19,37 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(email, password);
-    if (!user) {
-      throw new UnauthorizedException('You are not registered', 'Unauthorized');
+    if (this.blockedUsers.has(email)) {
+      throw new UnauthorizedException('Аккаунт заблокирован на 10 мин');
     }
+
+    const user = await this.authService.validateUser(email, password);
+
+    if (!user) {
+      const attempts = (this.failedLoginAttempts.get(email) || 0) + 1;
+      this.failedLoginAttempts.set(email, attempts);
+
+      if (attempts >= this.FAILE) {
+        this.blockUser(email);
+      }
+
+      throw new UnauthorizedException('Неудачная попытка входа.');
+    }
+
+    this.failedLoginAttempts.delete(email);
+
     return user;
   }
+
+  private blockUser(email: string): void {
+    this.blockedUsers.add(email);
+    setTimeout(() => {
+      this.unblockUser(email);
+    }, this.time);
+  }
+
+  private unblockUser(email: string): void {
+    this.blockedUsers.delete(email);
+    this.failedLoginAttempts.delete(email);
+}
 }
